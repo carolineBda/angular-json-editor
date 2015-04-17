34c3f1c8d96c4b81d5d56b65d7aa5872cb4825f5
carolineBda <caroline.bourdeu@gmail.com>
2015-04-17 15:51:19 +0100

    Clean up

--- demo/server.js
+++ demo/server.js
@@ -1,15 +1,9 @@
 var express  = require('express');
 var app      = express();
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/lib',  express.static(__dirname + '/../src/'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(methodOverride());
 
 app.listen(8080);
 console.log('App listening on port 8080');
